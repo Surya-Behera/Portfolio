52422f7cb2c090e90770e830a0a898bb5aaa817f
Surya-Behera <bbapuji866@gmail.com>
2025-02-01 22:31:11 +0530

    Initial commit

---
+++ components/about.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+       
+        I’m a passionate full-stack developer with a Master’s in Computer Applications, specializing in building dynamic, secure, and user-focused applications. With hands-on experience in ReactJS, Spring Boot, and MongoDB, I’ve created impactful projects like Stay Eazee and an AI-powered interview platform. Currently, as an Associate Software Developer at InvoLead, I craft innovative solutions using cutting-edge technologies. I thrive on solving complex problems, optimizing performance, and delivering high-quality software with a focus on real-time user experiences.
+      </p>
+
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        video games, watching movies, and playing with my dog. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about{" "}
+        <span className="font-medium">history and philosophy</span>. I'm also
+        learning how to play the guitar.
+      </p>
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,101 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import corpcommentImg from "@/public/corpcomment.png";
+import rmtdevImg from "@/public/rmtdev.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
+import liveprep from "@/public/liveprep.PNG";
+import chatbot from "@/public/chatbot.jpeg";
+import stayeazee from "@/public/stayeazee.PNG";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Associate Software Developer",
+    location: "Involead, Bhubaneswar, India",
+    description:
+      "I'm now a full-stack developer working in INVOLEAD,Bhubaneswar. My stack includes React, Next.js, TypeScript, Tailwind,Java,Springboot,MySQL and MongoDB.",
+    icon: React.createElement(FaReact),
+    date: "July 2024 - present",
+  },
+  {
+    title: "Intern Software Developer",
+    location: "Involead, Bhubaneswar, India",
+    description:
+      "I worked as a front-end developer. My stack includes React, Next.js, TypeScript, Tailwind,Java,Springboot,MySQL and MongoDB.",
+    icon: React.createElement(CgWorkAlt),
+    date: "Jan 2024 - July 2024",
+  }
+
+] as const;
+
+
+export const projectsData = [
+  {
+    title: "StayEazee",
+    description:
+    "This full-stack web application allows house owners to register their properties, while tenants can search and find the most suitable rooms available."
+,
+    tags: ["Next.js", "MySQL", "Tailwind", "Springboot"],
+    imageUrl:stayeazee,
+  },
+  {
+    title: "LIVE_prep AI interviewer (Voice interviewer)",
+    description:
+      "A React-based web application which allows students to practice for interviews with real-time questions.",
+    tags: ["React", "TypeScript", "MongoDB", "Tailwind"],
+    imageUrl: liveprep,
+  },
+  {
+    title: "CHAT BOT",
+    description:
+      "A website for an AI chatbot using Llama-2b model, serving as a virtual assistant.",
+    tags: ["React", "SQL", "Tailwind","Python","Framer"],
+    imageUrl: chatbot,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Git",
+  "Tailwind",
+  "MongoDB",
+  "Redux",
+  "Java",
+  "Springboot",
+  "MySQL",
+  "Python",
+  "Framer Motion",
+] as const;
